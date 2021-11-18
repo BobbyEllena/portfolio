8c6a021a44bd35292ee224128c9e92c38d9a9ef5
Bobby Ellena <bobbyellena@gmail.com>
2021-11-17 21:50:38 -0600

    placing files into sepertate folder for ease of use.

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Weather from "./Weather"
+import Weather from "./weather/Weather"
 import Me from './Me'
 
 ReactDOM.render(
